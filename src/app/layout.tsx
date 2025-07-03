85727a20c3b30f8fcf032791c8138fe5a014dedb
Vaibhav Jain <vaibhavtarun52@gmail.com>
2025-07-03 18:01:05 +0530

    Added google analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
+import Script from "next/script";
 import "./globals.css";
 import Navigation from "../components/Navigation";
 
@@ -17,6 +18,18 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en" className="scroll-smooth">
+      <head>
+        {/* Google tag (gtag.js) */}
+        <Script async src="https://www.googletagmanager.com/gtag/js?id=G-R1J3VW135K" />
+        <Script id="google-analytics">
+          {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', 'G-R1J3VW135K');
+          `}
+        </Script>
+      </head>
       <body className={inter.className}>
         <Navigation />
         <main className="relative">
